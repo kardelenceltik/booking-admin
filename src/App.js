68ceba7d3fe2fb65f31753be2445fde46e49ca02
Kardelen ÇELTİK <97738528+kardelenceltik@users.noreply.github.com>
2022-07-03 20:34:52 +0300

    navbar & sidebar configuration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import CategoriesList from "./components/CategoriesList";
 import CreateCategoryForm from "./components/CreateCategoryForm";
 import UserCreateForm from "./components/UserCreateForm";
 import UserList from "./components/UserList";
-
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div>
+      {/* <div>
+        <Navbar />
+      </div> */}
+
+      <div>
+        <Sidebar />
+      </div>
+      <div style={{ height: "75px" }}></div>
       <UserCreateForm />
       <UserList />
-      <CreateCategoryForm />
+      {/* <CreateCategoryForm /> */}
       <CategoriesList />
     </div>
   );
--- src/components/UserCreateForm.jsx
+++ src/components/UserCreateForm.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@mui/material/Fade";
+import TextField from "@mui/material/TextField";
 const UserCreateForm = () => {
   const [name, setName] = useState();
   const [surname, setSurname] = useState();
@@ -25,18 +28,71 @@ const UserCreateForm = () => {
       });
   };
   return (
-    <div>
-      <label>name:</label>
-      <input onChange={(e) => setName(e.target.value)} />
-      <label>surname:</label>
-      <input onChange={(e) => setSurname(e.target.value)} />
-      <label>mail:</label>
-      <input onChange={(e) => setMail(e.target.value)} />
-      <label>password:</label>
-      <input onChange={(e) => setPassword(e.target.value)} />
-      <label>phone:</label>
-      <input onChange={(e) => setPhone(e.target.value)} />
-      <button onClick={() => createUserHandler()}>enter</button>
+    <div className="user-create-form-container">
+      <div className="card user-create-form-card shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="card-body ">
+          <div className="form-group m-3">
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              onChange={(e) => setName(e.target.value)}
+              size="small"
+            />
+          </div>
+          <div className="form-group m-3">
+            <TextField
+              id="outlined-basic"
+              label="Surname"
+              variant="outlined"
+              onChange={(e) => setSurname(e.target.value)}
+              size="small"
+            />
+          </div>
+          <div className="form-group m-3">
+            <TextField
+              id="outlined-basic"
+              label="Mail"
+              variant="outlined"
+              onChange={(e) => setMail(e.target.value)}
+              size="small"
+            />
+          </div>
+          <div className="form-group m-3">
+            <TextField
+              id="outlined-basic "
+              label="Password"
+              variant="outlined"
+              onChange={(e) => setPassword(e.target.value)}
+              size="small"
+            />
+          </div>
+
+          <div className="form-group m-3">
+            <TextField
+              id="outlined-basic"
+              label="Phone"
+              variant="outlined"
+              onChange={(e) => setPhone(e.target.value)}
+              size="small"
+            />
+          </div>
+          <div className="form-group m-3">
+            <Fade in="true">
+              <Tooltip title="Click for save changes" placement="right">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => createUserHandler()}
+                  size="small"
+                >
+                  Save
+                </Button>
+              </Tooltip>
+            </Fade>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
