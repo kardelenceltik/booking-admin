ceead075102e4cd5a12d46b2791e396e3e918118
Kardelen ÇELTİK <97738528+kardelenceltik@users.noreply.github.com>
2022-07-04 23:55:42 +0300

    category managemen (add categort function added)

--- src/components/CategoryManagement.jsx
+++ src/components/CategoryManagement.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import axios from "axios";
 const CategoryManagement = () => {
   const [categories, setCategories] = useState([]);
+  const [name, setName] = useState();
+
   const getActiveCategoriesHandler = () => {
     axios
       .get("http://localhost:9292/categories/get-active-categories")
@@ -27,10 +31,29 @@ const CategoryManagement = () => {
         }
       });
   };
+  const createCategoryHandler = () => {
+    axios
+      .post("http://localhost:9292/categories/create-category", { name: name })
+      .then((response) => {
+        if (response.status === 200) {
+          getActiveCategoriesHandler();
+          alert("eklendi");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <div className="user-management-add-button-area">
-        <Button variant="outlined">Add</Button>
+        <Button
+          variant="outlined"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+        >
+          Add
+        </Button>
       </div>
       <table class="table">
         <thead>
@@ -63,6 +86,59 @@ const CategoryManagement = () => {
           })}
         </tbody>
       </table>
+      {/* <!-- Modal --> */}
+      <div
+        class="modal fade mt-5"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">
+                Create Category
+              </h5>
+
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <Box
+                component="form"
+                sx={{
+                  "& > :not(style)": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="standard-basic"
+                  label="Category Name"
+                  variant="standard"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Box>
+            </div>
+            <div class="modal-footer">
+              <Button variant="outlined">Close</Button>
+              <Button
+                variant="outlined"
+                onClick={() => createCategoryHandler()}
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/UserManagement.jsx
+++ src/components/UserManagement.jsx
@@ -16,7 +16,13 @@ const UserManagement = () => {
   return (
     <div>
       <div className="user-management-add-button-area">
-        <Button variant="outlined">Add</Button>
+        <Button
+          variant="outlined"
+          data-bs-toggle="modal"
+          data-bs-target=".exampleModal"
+        >
+          Add
+        </Button>
       </div>
       <table class="table">
         <thead>
@@ -52,6 +58,43 @@ const UserManagement = () => {
           })}
         </tbody>
       </table>
+
+      {/* <!-- Modal --> */}
+      <div
+        class="modal fade exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
